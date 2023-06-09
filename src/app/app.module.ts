351e03d87d3b1e91380b96985063c7375924936c
ws-malvagonzale\malvagonzalez <malvagonzalez@proxya.com>
2023-06-09 11:41:08 +0200

    added formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CabeceroComponent } from './cabecero/cabecero.component';
 import { IngresoComponent } from './ingreso/ingreso.component';
@@ -18,7 +18,8 @@ import { EgresoServicio } from './egreso/egreso.servicio';
     FormularioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [IngresoServicio,EgresoServicio],
   bootstrap: [AppComponent]
