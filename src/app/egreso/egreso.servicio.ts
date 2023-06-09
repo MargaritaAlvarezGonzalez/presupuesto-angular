3ef8add693f5bf2d4257f2309deb2f040906674a
ws-malvagonzale\malvagonzalez <malvagonzalez@proxya.com>
2023-06-09 08:48:54 +0200

    separeted components

---
+++ src/app/egreso/egreso.servicio.ts
@@ -0,0 +1,8 @@
+import { Egreso } from "./egreso.model";
+
+export class EgresoServicio{
+    egresos:Egreso[]=[
+        new Egreso("Renta Departamento", 900),
+        new Egreso("Ropa",200)
+    ];
+}
